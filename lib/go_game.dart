d39c36a279a08977fc25ae89349e1a1e43af41d9
Colby Natale <dbsmash@gmail.com>
2016-04-08 15:40:16 -0600

    moved some files

--- lib/go_game.dart
+++ lib/go_game.dart
@@ -11,6 +11,7 @@ import 'package:w_module/w_module.dart';
 export 'go_game.dart';
 
 part 'src/board_component.dart';
+part 'src/board.dart';
 part 'src/dot_component.dart';
 part 'src/actions.dart';
 part 'src/store.dart';
